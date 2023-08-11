6645baab82fcfcc24c4630d9a2a6fdaa8d536a1d
Alfredo <manjon.canela@gmail.com>
2023-08-11 14:16:37 +0200

    Acceptance criteria established

---
+++ tests/test_maze.py
@@ -0,0 +1,26 @@
+"""Module with data to use in tests"""
+from tests.data import maze1, maze2, maze3, maze4
+
+
+class TestClass:
+    """Class representing test from maze"""
+
+    def test_maze1(self):
+        """Testing bfs function with example 1"""
+        print(maze1)
+        assert False
+
+    def test_maze2(self):
+        """Testing bfs function with example 2"""
+        print(maze2)
+        assert False
+
+    def test_maze3(self):
+        """Testing bfs function with example 3"""
+        print(maze3)
+        assert False
+
+    def test_maze4(self):
+        """Testing bfs function with example 4"""
+        print(maze4)
+        assert False
