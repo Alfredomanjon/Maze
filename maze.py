0c794169321f382323e6b35ad6851f65e709555b
Alfredo <manjon.canela@gmail.com>
2023-08-12 14:40:49 +0200

    ended maze algorithm and refactored name of functions

--- maze.py
+++ maze.py
@@ -2,100 +2,117 @@ START_X = 0
 START_Y = 1
 
 
-def can_go_right(maze, y):
-    return len(maze[0]) - 1 >= y + 1
-
-
-def can_go_left(y):
-    return y - 1 >= 0
-
-
-def can_go_down(maze, x):
-    return len(maze) - 1 > x
-
-
-def can_go_up(x):
-    return x - 1 >= 0
-
-
 def is_free(content):
     return content != "#"
 
 
-def can_rotate(maze, x_position, y_position):
-    is_free_rotate = True
-    # arriba - izquierda
-    if can_go_up(x_position) and can_go_left(y_position):
-        is_free_rotate = is_free_rotate and (
+def can_go_right(maze, x, y, direction):
+    if direction == "horizontal":
+        return len(maze[0]) - 1 >= y + 2 and is_free(maze[x][y + 2])
+    return (
+        len(maze[0]) - 1 >= y + 1
+        and is_free(maze[x][y + 1])
+        and is_free(maze[x - 1][y + 1])
+        and is_free(maze[x + 1][y + 1])
+    )
+
+
+def can_go_left(maze, x, y, direction):
+    if direction == "horizontal":
+        return y - 2 >= 0 and is_free(maze[x][y - 2])
+    return (
+        y - 1 >= 0
+        and is_free(maze[x][y - 1])
+        and is_free(maze[x - 1][y - 1])
+        and is_free(maze[x + 1][y - 1])
+    )
+
+
+def can_go_down(maze, x, y, direction):
+    if direction == "horizontal":
+        return (
+            len(maze) - 1 > x
+            and is_free(maze[x + 1][y - 1])
+            and is_free(maze[x + 1][y])
+            and is_free(maze[x + 1][y + 1])
+        )
+    return len(maze) - 2 > x and is_free(maze[x + 2][y])
+
+
+def can_go_up(maze, x, y, direction):
+    if direction == "horizontal":
+        return (
+            x - 1 >= 0
+            and is_free(maze[x - 1][y - 1])
+            and is_free(maze[x - 1][y])
+            and is_free(maze[x - 1][y + 1])
+        )
+    return x - 2 >= 0 and is_free(maze[x - 2][y])
+
+
+def can_rotate(maze, x_position, y_position, direction):
+    if len(maze) - 2 > x_position >= 1 and len(maze[0]) - 2 > y_position >= 1:
+        if direction == "horizontal":
+            return (
+                is_free(maze[x_position - 1][y_position - 1])
+                and is_free(maze[x_position - 1][y_position])
+                and is_free(maze[x_position - 1][y_position - 1])
+                and is_free(maze[x_position + 1][y_position - 1])
+                and is_free(maze[x_position + 1][y_position])
+                and is_free(maze[x_position + 1][y_position + 1])
+            )
+        return (
             is_free(maze[x_position - 1][y_position - 1])
-            and is_free(maze[x_position - 1][y_position])
-            and is_free(maze[x_position][y_position - 1])
-        )
-    # arriba - derecha
-    if can_go_up(x_position) and can_go_right(maze, y_position):
-        is_free_rotate = is_free_rotate and (
-            is_free(maze[x_position - 1][y_position + 1])
-            and is_free(maze[x_position - 1][y_position])
+            and is_free(maze[x_position - 1][y_position - 1])
             and is_free(maze[x_position][y_position + 1])
-        )
-    # abajo - derecha
-    if can_go_down(maze, x_position) and can_go_right(maze, y_position):
-        is_free_rotate = is_free_rotate and (
-            is_free(maze[x_position + 1][y_position + 1])
-            and is_free(maze[x_position + 1][y_position])
-            and is_free(maze[x_position][y_position + 1])
-        )
-    # abajo - izquierda
-    if can_go_down(maze, x_position) and can_go_left(y_position):
-        is_free_rotate = is_free_rotate and (
-            is_free(maze[x_position + 1][y_position - 1])
-            and is_free(maze[x_position + 1][y_position])
             and is_free(maze[x_position][y_position - 1])
+            and is_free(maze[x_position + 1][y_position - 1])
+            and is_free(maze[x_position + 1][y_position + 1])
         )
 
-    return is_free_rotate
 
-
-def maze_bfs(maze):
+def find_shortest_distance(maze):
     """BFS Algorithm"""
-    end_x = len(maze) - 2
-    end_y = len(maze[0]) - 1
-    
-    frontier = []
-    frontier.append((START_X, START_Y, "right"))
+    end_x_vertical = len(maze) - 2
+    end_y_vertical = len(maze[0]) - 1
+    end_x_horizontal = len(maze) - 1
+    end_y_horizontal = len(maze[0]) - 2
+
+    posibilities = []
+    posibilities.append((START_X, START_Y, "horizontal"))
     visited = []
     counter = 0
 
-    while len(frontier) > 0:
-        temp_frontier = []
-        for node in frontier:
+    while len(posibilities) > 0:
+        temp_possibilities = []
+        for node in posibilities:
             if node not in visited:
-                if (node[0], node[1]) == (end_x, end_y):
-                    return counter + 1
-
                 x_position = node[0]
                 y_position = node[1]
                 direction = node[2]
-                if can_rotate(maze, x_position, y_position):
-                    if can_go_right(maze, y_position):
-                        temp_frontier.append((x_position, y_position + 1, "right"))
-                    if can_go_left(y_position):
-                        temp_frontier.append((x_position, y_position - 1, "left"))
-                    if can_go_down(maze, x_position):
-                        temp_frontier.append((x_position + 1, y_position, "down"))
-                    if can_go_up(x_position):
-                        temp_frontier.append((x_position - 1, y_position, "up"))
-                else:
-                    if direction == "down" and can_go_down(maze, x_position):
-                        temp_frontier.append((x_position + 1, y_position, "down"))
-                    if direction == "right" and can_go_right(maze, y_position):
-                        temp_frontier.append((x_position, y_position + 1, "right"))
-                    if direction == "up" and can_go_up(x_position):
-                        temp_frontier.append((x_position - 1, y_position, "up"))
-                    if direction == "left" and can_go_left(y_position):
-                        temp_frontier.append((x_position, y_position - 1, "left"))
-        visited.extend(frontier)
-        frontier = temp_frontier
+                if (x_position, y_position) == (end_x_vertical, end_y_vertical) or (
+                    x_position,
+                    y_position,
+                ) == (end_x_horizontal, end_y_horizontal):
+                    return counter
+                if can_rotate(maze, x_position, y_position, direction):
+                    temp_possibilities.append(
+                        (
+                            x_position,
+                            y_position,
+                            "horizontal" if direction == "vertical" else "vertical",
+                        )
+                    )
+                if can_go_down(maze, x_position, y_position, direction):
+                    temp_possibilities.append((x_position + 1, y_position, direction))
+                if can_go_right(maze, x_position, y_position, direction):
+                    temp_possibilities.append((x_position, y_position + 1, direction))
+                if can_go_up(maze, x_position, y_position, direction):
+                    temp_possibilities.append((x_position - 1, y_position, direction))
+                if can_go_left(maze, x_position, y_position, direction):
+                    temp_possibilities.append((x_position, y_position - 1, direction))
+        visited.extend(posibilities)
+        posibilities = temp_possibilities
         counter += 1
 
     return -1
--- tests/test_maze.py
+++ tests/test_maze.py
@@ -1,23 +1,22 @@
 """Module with data to use in tests"""
 from tests.data import maze1, maze2, maze3, maze4
-from maze import maze_bfs
+from maze import find_shortest_distance
 
 class TestClass:
     """Class representing test from maze"""
 
     def test_maze1(self):
         """Testing bfs function with example 1"""
-        assert maze_bfs(maze1) == 11
+        assert find_shortest_distance(maze1) == 11
 
     def test_maze2(self):
         """Testing bfs function with example 2"""
-        assert maze_bfs(maze2) == -1
+        assert find_shortest_distance(maze2) == -1
 
     def test_maze3(self):
         """Testing bfs function with example 3"""
-        assert maze_bfs(maze3) == 2
+        assert find_shortest_distance(maze3) == 2
 
     def test_maze4(self):
         """Testing bfs function with example 4"""
-        assert maze_bfs(maze4) == 16
-        
+        assert find_shortest_distance(maze4) == 16
