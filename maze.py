9918be3f2d9070618a6a86347f8c433615856381
Alfredo <manjon.canela@gmail.com>
2023-08-12 11:41:57 +0200

    First implementation of maze

--- maze.py
+++ maze.py
@@ -2,41 +2,65 @@ START_X = 0
 START_Y = 1
 
 
-def can_rotate(maze, x, y):
-    return (
-        maze[x - 1][y - 1] != "#"
-        and maze[x - 1][y] != "#"
-        and maze[x - 1][y - 1] != "#"
-        and maze[x][y + 1] != "#"
-        and maze[x][y] != "#"
-        and maze[x][y - 1] != "#"
-        and maze[x + 1][y - 1] != "#"
-        and maze[x + 1][y] != "#"
-        and maze[x + 1][y + 1] != "#"
-    )
+def can_go_right(maze, y):
+    return len(maze[0]) - 1 >= y + 1
 
 
-def can_go_right(maze, x):
-    return len(maze[0]) - 1 < x + 1
+def can_go_left(y):
+    return y - 1 >= 0
 
 
-def can_go_left(x):
-    return x - 1 > 0
+def can_go_down(maze, x):
+    return len(maze) - 1 > x
 
 
-def can_go_down(maze, y):
-    return len(maze) - 1 > y
+def can_go_up(x):
+    return x - 1 >= 0
 
 
-def can_go_up(y):
-    return y - 1 > 0
+def is_free(content):
+    return content != "#"
+
+
+def can_rotate(maze, x_position, y_position):
+    is_free_rotate = True
+    # arriba - izquierda
+    if can_go_up(x_position) and can_go_left(y_position):
+        is_free_rotate = is_free_rotate and (
+            is_free(maze[x_position - 1][y_position - 1])
+            and is_free(maze[x_position - 1][y_position])
+            and is_free(maze[x_position][y_position - 1])
+        )
+    # arriba - derecha
+    if can_go_up(x_position) and can_go_right(maze, y_position):
+        is_free_rotate = is_free_rotate and (
+            is_free(maze[x_position - 1][y_position + 1])
+            and is_free(maze[x_position - 1][y_position])
+            and is_free(maze[x_position][y_position + 1])
+        )
+    # abajo - derecha
+    if can_go_down(maze, x_position) and can_go_right(maze, y_position):
+        is_free_rotate = is_free_rotate and (
+            is_free(maze[x_position + 1][y_position + 1])
+            and is_free(maze[x_position + 1][y_position])
+            and is_free(maze[x_position][y_position + 1])
+        )
+    # abajo - izquierda
+    if can_go_down(maze, x_position) and can_go_left(y_position):
+        is_free_rotate = is_free_rotate and (
+            is_free(maze[x_position + 1][y_position - 1])
+            and is_free(maze[x_position + 1][y_position])
+            and is_free(maze[x_position][y_position - 1])
+        )
+
+    return is_free_rotate
 
 
 def maze_bfs(maze):
     """BFS Algorithm"""
     end_x = len(maze) - 2
     end_y = len(maze[0]) - 1
-
+    
     frontier = []
     frontier.append((START_X, START_Y, "right"))
     visited = []
@@ -46,30 +70,30 @@ def maze_bfs(maze):
         temp_frontier = []
         for node in frontier:
             if node not in visited:
-                if node == (end_x, end_y):
+                if (node[0], node[1]) == (end_x, end_y):
                     return counter + 1
 
                 x_position = node[0]
                 y_position = node[1]
                 direction = node[2]
                 if can_rotate(maze, x_position, y_position):
-                    if can_go_right(maze, x_position):
-                        temp_frontier.append((x_position + 1, y_position, "right"))
-                    if can_go_left(x_position):
-                        temp_frontier.append((x_position - 1, y_position, "left"))
-                    if can_go_down(maze, y_position):
-                        temp_frontier.append((x_position, y_position + 1, "down"))
-                    if can_go_up(y_position):
-                        temp_frontier.append((x_position, y_position - 1, "up"))
+                    if can_go_right(maze, y_position):
+                        temp_frontier.append((x_position, y_position + 1, "right"))
+                    if can_go_left(y_position):
+                        temp_frontier.append((x_position, y_position - 1, "left"))
+                    if can_go_down(maze, x_position):
+                        temp_frontier.append((x_position + 1, y_position, "down"))
+                    if can_go_up(x_position):
+                        temp_frontier.append((x_position - 1, y_position, "up"))
                 else:
-                    if direction == "down":
-                        temp_frontier.append((x_position, y_position + 1, "down"))
-                    if direction == "right":
-                        temp_frontier.append((x_position + 1, y_position, "right"))
-                    if direction == "up":
-                        temp_frontier.append((x_position, y_position - 1, "up"))
-                    if direction == "left":
-                        temp_frontier.append((x_position - 1, y_position, "left"))
+                    if direction == "down" and can_go_down(maze, x_position):
+                        temp_frontier.append((x_position + 1, y_position, "down"))
+                    if direction == "right" and can_go_right(maze, y_position):
+                        temp_frontier.append((x_position, y_position + 1, "right"))
+                    if direction == "up" and can_go_up(x_position):
+                        temp_frontier.append((x_position - 1, y_position, "up"))
+                    if direction == "left" and can_go_left(y_position):
+                        temp_frontier.append((x_position, y_position - 1, "left"))
         visited.extend(frontier)
         frontier = temp_frontier
         counter += 1
